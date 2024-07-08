bd4ee74f3f3316464e7c33b34ca0e9eaef2f72c0
Hiruni <kathaluwagehiruni@gmail.com>
2024-07-08 20:52:29 +0100

    Fixed the css path

--- User/userdashboard.php
+++ User/userdashboard.php
@@ -76,7 +76,7 @@ $conn->close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type='text/css' media='screen' href="userprofile.css">
+    <link rel="stylesheet" type='text/css' media='screen' href="userdashboard.css">
 
     <title>My Profile</title>
 
