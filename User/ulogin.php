5663762dac065bfa67204f722424b0470d27756e
Hiruni <kathaluwagehiruni@gmail.com>
2024-07-09 10:58:11 +0100

    Fixed CSS path

--- User/ulogin.php
+++ User/ulogin.php
@@ -49,7 +49,7 @@ if(isset($_POST["submit"]))
     <meta charset="UTF-8">
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <meta name="viewport" content="width=device-width" initial-scale="1.0">
-    <link rel="stylesheet" type='text/css' media='screen' href="Login.css">
+    <link rel="stylesheet" type='text/css' media='screen' href="ulogin.css">
     <link rel="stylesheet" href="header_footer.css">
 
     <title>Customer Login</title>
